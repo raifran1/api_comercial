9f84f89461af016cf4c9bfdabc0eee71d46c04a9
raifran1 <raifranlucas@outlook.com>
2020-07-03 21:55:44 -0300

    app de produto

--- app/venda/models.py
+++ app/venda/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Venda(models.Model):
+    funcionario = models.ForeignKey('funcionario.Funcionario', on_delete=models.CASCADE, verbose_name='Funcionário')
+    cliente = models.ForeignKey('cliente.Cliente', on_delete=models.CASCADE, verbose_name='Cliente')
+    criado_em = models.DateField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Venda'
+        verbose_name_plural = 'Vendas'
+
+class VendasProd(models.Model):
+    venda = models.ForeignKey(Venda, on_delete=models.CASCADE, verbose_name='Venda')
+    produto = models.ForeignKey('produto.Produto', on_delete=models.CASCADE, verbose_name='Produto')
+    quantidade = models.PositiveIntegerField(verbose_name='Quantidade', null=False, default=1)
+
+    class Meta:
+        verbose_name = 'Produto da venda'
+        verbose_name_plural = 'Produtos da venda'
